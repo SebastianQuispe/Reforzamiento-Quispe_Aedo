88703810b827c7664f01d97b15c16ae86002c366
SebastianQuispe <sebastianrosendo.quispe@unmsm.edu.pe>
2024-09-07 02:04:16 -0500

    Add files via upload

---
+++ Reforzamiento 3/Reforzamiento 3/Diccionario/02.py
@@ -0,0 +1,2 @@
+var1={"nombre":"Sebastian","Edad":21,"Salario":1500}
+print(list(var1.items()))
---
+++ Reforzamiento 3/Reforzamiento 3/Diccionario/05.py
@@ -0,0 +1,8 @@
+var1={"departamento1":"Lima",
+      "departamento2":"Moquegua",
+      "departamento3":"Cuzco",
+      "departamento4":"Ucayali",
+      "departamento5":"Loreto"}
+
+del var1["departamento1"]
+print(var1)
